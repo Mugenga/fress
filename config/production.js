2b6cabdd786860755f377deb24f74c32cbab86c2
mugenga <rockyyves@gmail.com>
2021-11-27 20:54:09 +0200

    Environment Variables Changes

---
+++ config/production.js
@@ -0,0 +1,11 @@
+module.exports = {
+    app: {
+      name: "Fress Media",
+      port: "3000",
+      node_env: "production",
+      saltRounds: 3,
+      jwtKey: "1516239022",
+      jwtKeyExpirationTime: 100 * 24 * 24
+    }
+  };
+  
--- src/controllers/sendgrid.js
+++ src/controllers/sendgrid.js
@@ -1,7 +1,11 @@
 const config = require("config");
 const logger = require("../logging");
 const sendgrid = require("@sendgrid/mail");
-sendgrid.setApiKey(config.get("app.SENDGRID_API_KEY"));
+sendgrid.setApiKey(
+  process.env.NODE_ENV !== "production"
+    ? `${config.get("db.url")}`
+    : process.env.SENDGRID_API_KEY
+);
 
 const sendmailWithSendGrid = async (to, subject, html) => {
   try {
