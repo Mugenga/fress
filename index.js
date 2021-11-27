b4dd2ad26a93c94af5ad1d3993598e304d9b3731
mugenga <rockyyves@gmail.com>
2021-11-27 21:16:39 +0200

    Environment Variables Changes

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const config = require('config');
-const cors = require('cors');
-const logger = require('./src/logging');
-const api = require('./src/routes');
+const express = require("express");
+const config = require("config");
+const cors = require("cors");
+const logger = require("./src/logging");
+const api = require("./src/routes");
 const app = express();
-const mongoCon = require('./src/startup/mongo');
+const mongoCon = require("./src/startup/mongo");
 const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
 
 // Disable Powered By Header
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // Allow cors from dev environment
-if (config.get('app.node_env') === 'development') {
+if (config.get("app.node_env") === "development") {
   //allowing cors policies
   app.use(cors());
 }
 
 app.use(
   express.json({
-    limit: '5mb'
+    limit: "5mb",
   })
 );
 app.use(express.static("./public"));
-app.use(bodyParser.json({limit: "20mb"}));
+app.use(bodyParser.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 mongoCon();
@@ -30,14 +31,7 @@ mongoCon();
 // The routes ...
 
 api.mountRoutes(app);
-// app.use(
-//   fileUpload({
-//     limits: { fileSize: 50 * 1024 * 1024 }
-//   })
-// );
 
-app.listen(3000, () =>
-  logger.info(
-    `${config.get('app.name')} service is listening on port 3000 !`
-  )
+app.listen(port, () =>
+  logger.info(`${config.get("app.name")} service is listening on port 3000 !`)
 );
